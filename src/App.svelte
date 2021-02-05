2b5cc24a56abb80726b68984ff82e96e9b4704d3
Joan Andrés <andres6936@live.com>
2021-02-05 00:40:14 -0500

    Moved script

--- src/App.svelte
+++ src/App.svelte
@@ -516,35 +516,6 @@
 	});
 	// @formatter:on
 
-	// @formatter:off
-	document.addEventListener("DOMContentLoaded", function () {
-		window.ApexCharts && (new ApexCharts(document.getElementById('sparkline-bounce-rate-6'), {
-			chart: {
-				type: "line",
-				fontFamily: 'inherit',
-				height: 24,
-				animations: {
-					enabled: false
-				},
-				sparkline: {
-					enabled: true
-				},
-			},
-			tooltip: {
-				enabled: false,
-			},
-			stroke: {
-				width: 2,
-				lineCap: "round",
-			},
-			series: [{
-				color: "#206bc4",
-				data: [22, 12, 7, 14, 3, 21, 8, 23, 18, 14]
-			}],
-		})).render();
-	});
-	// @formatter:on
-
 	import Task from "./Task.svelte";
 	import Sales from './Sales.svelte';
 	import Header from './Header.svelte';
--- src/MostVisitedPages.svelte
+++ src/MostVisitedPages.svelte
@@ -1,3 +1,34 @@
+<script>
+    // @formatter:off
+    document.addEventListener("DOMContentLoaded", function () {
+        window.ApexCharts && (new ApexCharts(document.getElementById('sparkline-bounce-rate-6'), {
+            chart: {
+                type: "line",
+                fontFamily: 'inherit',
+                height: 24,
+                animations: {
+                    enabled: false
+                },
+                sparkline: {
+                    enabled: true
+                },
+            },
+            tooltip: {
+                enabled: false,
+            },
+            stroke: {
+                width: 2,
+                lineCap: "round",
+            },
+            series: [{
+                color: "#206bc4",
+                data: [22, 12, 7, 14, 3, 21, 8, 23, 18, 14]
+            }],
+        })).render();
+    });
+    // @formatter:on
+</script>
+
 <div class="card">
     <div class="card-header">
         <h3 class="card-title">Most Visited Pages</h3>
