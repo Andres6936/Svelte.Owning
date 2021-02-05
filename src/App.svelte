7de5f67f14d3a35d09f48cbbabadccbc95ae6861
Joan Andrés <andres6936@live.com>
2021-02-04 23:31:41 -0500

    Created page title component

--- src/App.svelte
+++ src/App.svelte
@@ -762,6 +762,7 @@
 
 <script>
 	import Header from './Header.svelte';
+	import PageTitle from './PageTitle.svelte';
 	import Navigation from './Navigation.svelte';
 </script>
 
@@ -770,51 +771,7 @@
 	<Navigation/>
 	<div class="content">
 		<div class="container-xl">
-			<!-- Page title -->
-			<div class="page-header d-print-none">
-				<div class="row align-items-center">
-					<div class="col">
-						<!-- Page pre-title -->
-						<div class="page-pretitle">
-							Overview
-						</div>
-						<h2 class="page-title">
-							Dashboard
-						</h2>
-					</div>
-					<!-- Page title actions -->
-					<div class="col-auto ms-auto d-print-none">
-						<div class="btn-list">
-                  <span class="d-none d-sm-inline">
-                    <a href="#" class="btn btn-white">
-                      New view
-                    </a>
-                  </span>
-							<a href="#" class="btn btn-primary d-none d-sm-inline-block" data-bs-toggle="modal"
-							   data-bs-target="#modal-report">
-								<svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
-									 viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-									 stroke-linecap="round" stroke-linejoin="round">
-									<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-									<line x1="12" y1="5" x2="12" y2="19"/>
-									<line x1="5" y1="12" x2="19" y2="12"/>
-								</svg>
-								Create new report
-							</a>
-							<a href="#" class="btn btn-primary d-sm-none btn-icon" data-bs-toggle="modal"
-							   data-bs-target="#modal-report" aria-label="Create new report">
-								<svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
-									 viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-									 stroke-linecap="round" stroke-linejoin="round">
-									<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-									<line x1="12" y1="5" x2="12" y2="19"/>
-									<line x1="5" y1="12" x2="19" y2="12"/>
-								</svg>
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>
+			<PageTitle/>
 			<div class="row row-deck row-cards">
 				<div class="col-sm-6 col-lg-3">
 					<div class="card">
