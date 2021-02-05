4c8de3a384066e7c3ab08f1c9aff53663f53c26a
Joan Andrés <andres6936@live.com>
2021-02-04 22:29:57 -0500

    Added form

---
+++ src/InputLabel.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let id;
+    export let type;
+    export let text;
+</script>
+
+<label class="form-label">
+    {text}
+    <input {type} class="form-control" {id}>
+</label>
---
+++ src/Login.svelte
@@ -0,0 +1,18 @@
+<script>
+    import InputLabel from './InputLabel.svelte';
+</script>
+
+<form>
+    <div class="mb-3">
+        <InputLabel text="Email address" type="email" id="svo-input-email"/>
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+    <div class="mb-3">
+        <InputLabel text="Password" type="password" id="svo-input-password"/>
+    </div>
+    <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
