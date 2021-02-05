97a8b93a1cf954950ad768d870d08299aa90ea6b
Joan Andrés <andres6936@live.com>
2021-02-05 16:34:53 -0500

    Created the Daily Resume and svg Dollar Sign

--- src/App.svelte
+++ src/App.svelte
@@ -19,6 +19,8 @@
 	import SocialMediaTraffic from "./SocialMediaTraffic.svelte";
 	import DevelopmentActivity from "./DevelopmentActivity.svelte";
 	import ComparativeResume from "./Component/ComparativeResume.svelte";
+	import DailyResume from "./Component/DailyResume.svelte";
+	import DolarSign from "./SVG/DolarSign.svelte";
 </script>
 
 <div class="page">
@@ -82,32 +84,9 @@
 				<div class="col-md-4">
 					<div class="row row-cards">
 						<div class="col-12">
-							<div class="card card-sm">
-								<div class="card-body">
-									<div class="row align-items-center">
-										<div class="col-auto">
-                          <span class="bg-blue text-white avatar"><svg xmlns="http://www.w3.org/2000/svg" class="icon"
-																	   width="24" height="24" viewBox="0 0 24 24"
-																	   stroke-width="2" stroke="currentColor"
-																	   fill="none" stroke-linecap="round"
-																	   stroke-linejoin="round"><path stroke="none"
-																									 d="M0 0h24v24H0z"
-																									 fill="none"/><path
-								  d="M16.7 8a3 3 0 0 0 -2.7 -2h-4a3 3 0 0 0 0 6h4a3 3 0 0 1 0 6h-4a3 3 0 0 1 -2.7 -2"/><path
-								  d="M12 3v3m0 12v3"/></svg>
-                          </span>
-										</div>
-										<div class="col">
-											<div class="font-weight-medium">
-												132 Sales
-											</div>
-											<div class="text-muted">
-												12 waiting payments
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
+							<DailyResume titleResume="132 Sales" growthResume="12 waiting payments">
+								<DolarSign/>
+							</DailyResume>
 						</div>
 						<div class="col-12">
 							<div class="card card-sm">
---
+++ src/Component/DailyResume.svelte
@@ -0,0 +1,26 @@
+<script>
+    // @type {string} Indicate the title of daily resume.
+    export let titleResume;
+    // @type {string} Indicate the amount of growth obtained today.
+    export let growthResume;
+</script>
+
+<div class="card card-sm">
+    <div class="card-body">
+        <div class="row align-items-center">
+            <div class="col-auto">
+                  <span class="bg-blue text-white avatar">
+                      <slot/>
+                  </span>
+            </div>
+            <div class="col">
+                <div class="font-weight-medium">
+                    {titleResume}
+                </div>
+                <div class="text-muted">
+                    {growthResume}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
