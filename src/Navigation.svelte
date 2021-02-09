16a983844b7efc955c0a96492be787efd1fb7a61
Joan Andrés <andres6936@live.com>
2021-02-09 14:08:26 -0500

    Created new component Svg24

--- src/Navigation.svelte
+++ src/Navigation.svelte
@@ -1,3 +1,6 @@
+<script>
+    import Svg24 from "./SVG/Svg24.svelte";
+</script>
 <div class="navbar-expand-md">
     <div class="collapse navbar-collapse" id="navbar-menu">
         <div class="navbar navbar-light">
@@ -5,15 +8,13 @@
                 <ul class="navbar-nav">
                     <li class="nav-item active">
                         <a class="nav-link" href="/">
-                    <span class="nav-link-icon d-md-none d-lg-inline-block"><svg xmlns="http://www.w3.org/2000/svg"
-                                                                                 class="icon" width="24" height="24"
-                                                                                 viewBox="0 0 24 24" stroke-width="2"
-                                                                                 stroke="currentColor" fill="none"
-                                                                                 stroke-linecap="round"
-                                                                                 stroke-linejoin="round"><path
-                            stroke="none" d="M0 0h24v24H0z" fill="none"/><polyline points="5 12 3 12 12 3 21 12 19 12"/><path
-                            d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"/><path
-                            d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"/></svg>
+                    <span class="nav-link-icon d-md-none d-lg-inline-block">
+                        <Svg24>
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                            <polyline points="5 12 3 12 12 3 21 12 19 12"/>
+                            <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"/>
+                            <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"/>
+                        </Svg24>
                     </span>
                             <span class="nav-link-title">
                       Home
@@ -23,18 +24,15 @@
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#navbar-base" data-bs-toggle="dropdown"
                            role="button" aria-expanded="false">
-                    <span class="nav-link-icon d-md-none d-lg-inline-block"><svg xmlns="http://www.w3.org/2000/svg"
-                                                                                 class="icon" width="24" height="24"
-                                                                                 viewBox="0 0 24 24" stroke-width="2"
-                                                                                 stroke="currentColor" fill="none"
-                                                                                 stroke-linecap="round"
-                                                                                 stroke-linejoin="round"><path
-                            stroke="none" d="M0 0h24v24H0z" fill="none"/><polyline
-                            points="12 3 20 7.5 20 16.5 12 21 4 16.5 4 7.5 12 3"/><line x1="12" y1="12" x2="20"
-                                                                                        y2="7.5"/><line x1="12" y1="12"
-                                                                                                        x2="12"
-                                                                                                        y2="21"/><line
-                            x1="12" y1="12" x2="4" y2="7.5"/><line x1="16" y1="5.25" x2="8" y2="9.75"/></svg>
+                    <span class="nav-link-icon d-md-none d-lg-inline-block">
+                        <Svg24>
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                            <polyline points="12 3 20 7.5 20 16.5 12 21 4 16.5 4 7.5 12 3"/>
+                            <line x1="12" y1="12" x2="20" y2="7.5"/>
+                            <line x1="12" y1="12" x2="12" y2="21"/>
+                            <line x1="12" y1="12" x2="4" y2="7.5"/>
+                            <line x1="16" y1="5.25" x2="8" y2="9.75"/>
+                        </Svg24>
                     </span>
                             <span class="nav-link-title">
                       Interface
@@ -153,14 +151,12 @@
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="form-elements">
-                    <span class="nav-link-icon d-md-none d-lg-inline-block"><svg xmlns="http://www.w3.org/2000/svg"
-                                                                                 class="icon" width="24" height="24"
-                                                                                 viewBox="0 0 24 24" stroke-width="2"
-                                                                                 stroke="currentColor" fill="none"
-                                                                                 stroke-linecap="round"
-                                                                                 stroke-linejoin="round"><path
-                            stroke="none" d="M0 0h24v24H0z" fill="none"/><polyline points="9 11 12 14 20 6"/><path
-                            d="M20 12v6a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h9"/></svg>
+                    <span class="nav-link-icon d-md-none d-lg-inline-block">
+                        <Svg24>
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                            <polyline points="9 11 12 14 20 6"/>
+                            <path d="M20 12v6a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h9"/>
+                        </Svg24>
                     </span>
                             <span class="nav-link-title">
                       Form elements
@@ -170,14 +166,11 @@
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#navbar-extra" data-bs-toggle="dropdown"
                            role="button" aria-expanded="false">
-                    <span class="nav-link-icon d-md-none d-lg-inline-block"><svg xmlns="http://www.w3.org/2000/svg"
-                                                                                 class="icon" width="24" height="24"
-                                                                                 viewBox="0 0 24 24" stroke-width="2"
-                                                                                 stroke="currentColor" fill="none"
-                                                                                 stroke-linecap="round"
-                                                                                 stroke-linejoin="round"><path
-                            stroke="none" d="M0 0h24v24H0z" fill="none"/><path
-                            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"/></svg>
+                    <span class="nav-link-icon d-md-none d-lg-inline-block">
+                        <Svg24>
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                            <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"/>
+                        </Svg24>
                     </span>
                             <span class="nav-link-title">
                       Extra
@@ -219,15 +212,14 @@
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#navbar-layout" data-bs-toggle="dropdown"
                            role="button" aria-expanded="false">
-                    <span class="nav-link-icon d-md-none d-lg-inline-block"><svg xmlns="http://www.w3.org/2000/svg"
-                                                                                 class="icon" width="24" height="24"
-                                                                                 viewBox="0 0 24 24" stroke-width="2"
-                                                                                 stroke="currentColor" fill="none"
-                                                                                 stroke-linecap="round"
-                                                                                 stroke-linejoin="round"><path
-                            stroke="none" d="M0 0h24v24H0z" fill="none"/><rect x="4" y="4" width="6" height="5" rx="2"/><rect
-                            x="4" y="13" width="6" height="7" rx="2"/><rect x="14" y="4" width="6" height="7" rx="2"/><rect
-                            x="14" y="15" width="6" height="5" rx="2"/></svg>
+                    <span class="nav-link-icon d-md-none d-lg-inline-block">
+                        <Svg24>
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                            <rect x="4" y="4" width="6" height="5" rx="2"/>
+                            <rect x="4" y="13" width="6" height="7" rx="2"/>
+                            <rect x="14" y="4" width="6" height="7" rx="2"/>
+                            <rect x="14" y="15" width="6" height="5" rx="2"/>
+                        </Svg24>
                     </span>
                             <span class="nav-link-title">
                       Layout
@@ -286,18 +278,15 @@
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="docs/index">
-                    <span class="nav-link-icon d-md-none d-lg-inline-block"><svg xmlns="http://www.w3.org/2000/svg"
-                                                                                 class="icon" width="24" height="24"
-                                                                                 viewBox="0 0 24 24" stroke-width="2"
-                                                                                 stroke="currentColor" fill="none"
-                                                                                 stroke-linecap="round"
-                                                                                 stroke-linejoin="round"><path
-                            stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 3v4a1 1 0 0 0 1 1h4"/><path
-                            d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"/><line x1="9"
-                                                                                                              y1="9"
-                                                                                                              x2="10"
-                                                                                                              y2="9"/><line
-                            x1="9" y1="13" x2="15" y2="13"/><line x1="9" y1="17" x2="15" y2="17"/></svg>
+                    <span class="nav-link-icon d-md-none d-lg-inline-block">
+                        <Svg24>
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                            <path d="M14 3v4a1 1 0 0 0 1 1h4"/>
+                            <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"/>
+                            <line x1="9" y1="9" x2="10" y2="9"/>
+                            <line x1="9" y1="13" x2="15" y2="13"/>
+                            <line x1="9" y1="17" x2="15" y2="17"/>
+                        </Svg24>
                     </span>
                             <span class="nav-link-title">
                       Documentation
@@ -309,12 +298,11 @@
                     <form action="." method="get">
                         <div class="input-icon">
                     <span class="input-icon-addon">
-                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
-                           stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                           stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="10"
-                                                                                                              cy="10"
-                                                                                                              r="7"/><line
-                              x1="21" y1="21" x2="15" y2="15"/></svg>
+                      <Svg24>
+                          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                          <circle cx="10" cy="10" r="7"/>
+                          <line x1="21" y1="21" x2="15" y2="15"/>
+                      </Svg24>
                     </span>
                             <input type="text" class="form-control" placeholder="Search…"
                                    aria-label="Search in website">
