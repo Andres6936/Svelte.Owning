6e9c31c12b46c35e362cb0fe7119c33e8f81ec47
Joan Andrés <andres6936@live.com>
2021-02-09 14:18:20 -0500

    Use of Svg24

--- src/Page/Blank.svelte
+++ src/Page/Blank.svelte
@@ -1,3 +1,6 @@
+<script>
+    import Svg24 from "../SVG/Svg24.svelte";
+</script>
 <div class="container-xl d-flex flex-column justify-content-center">
     <div class="empty">
         <div class="empty-img"><img src="./static/illustrations/undraw_printing_invoices_5r4r.svg" height="128"
@@ -9,13 +12,11 @@
         </p>
         <div class="empty-action">
             <a href="./." class="btn btn-primary">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
-                     stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                     stroke-linejoin="round">
+                <Svg24>
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <line x1="12" y1="5" x2="12" y2="19"/>
                     <line x1="5" y1="12" x2="19" y2="12"/>
-                </svg>
+                </Svg24>
                 Add your first client
             </a>
         </div>
