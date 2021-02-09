25434efbc6296f478a65bfb57abf15a9bb9beb2b
Joan Andrés <andres6936@live.com>
2021-02-09 17:59:38 -0500

    Use of ApexChart

--- src/Revenue.svelte
+++ src/Revenue.svelte
@@ -1,64 +1,78 @@
 <script>
-    // @formatter:off
-    document.addEventListener("DOMContentLoaded", function () {
-        window.ApexCharts && (new ApexCharts(document.getElementById('chart-revenue-bg'), {
-            chart: {
-                type: "area",
-                fontFamily: 'inherit',
-                height: 40.0,
-                sparkline: {
-                    enabled: true
-                },
-                animations: {
-                    enabled: false
-                },
+    import ApexCharts from './Chart/Apexchart/index.min.js';
+
+    export const renderApexChart = (node, options) => {
+        const apexChart = new ApexCharts(node, options);
+        apexChart.render();
+
+        return {
+            update(options) {
+                apexChart.updateOptions(options);
             },
-            dataLabels: {
-                enabled: false,
+            destroy() {
+                apexChart.destroy();
+            }
+        }
+    };
+
+
+    const chartRevenueBg = {
+        chart: {
+            type: "area",
+            fontFamily: 'inherit',
+            height: 40.0,
+            sparkline: {
+                enabled: true
             },
-            fill: {
-                opacity: .16,
-                type: 'solid'
+            animations: {
+                enabled: false
             },
-            stroke: {
-                width: 2,
-                lineCap: "round",
-                curve: "smooth",
+        },
+        dataLabels: {
+            enabled: false,
+        },
+        fill: {
+            opacity: .16,
+            type: 'solid'
+        },
+        stroke: {
+            width: 2,
+            lineCap: "round",
+            curve: "smooth",
+        },
+        series: [{
+            name: "Profits",
+            data: [37, 35, 44, 28, 36, 24, 65, 31, 37, 39, 62, 51, 35, 41, 35, 27, 93, 53, 61, 27, 54, 43, 19, 46, 39, 62, 51, 35, 41, 67]
+        }],
+        grid: {
+            strokeDashArray: 4,
+        },
+        xaxis: {
+            labels: {
+                padding: 0
             },
-            series: [{
-                name: "Profits",
-                data: [37, 35, 44, 28, 36, 24, 65, 31, 37, 39, 62, 51, 35, 41, 35, 27, 93, 53, 61, 27, 54, 43, 19, 46, 39, 62, 51, 35, 41, 67]
-            }],
-            grid: {
-                strokeDashArray: 4,
+            tooltip: {
+                enabled: false
             },
-            xaxis: {
-                labels: {
-                    padding: 0
-                },
-                tooltip: {
-                    enabled: false
-                },
-                axisBorder: {
-                    show: false,
-                },
-                type: 'datetime',
-            },
-            yaxis: {
-                labels: {
-                    padding: 4
-                },
-            },
-            labels: [
-                '2020-06-20', '2020-06-21', '2020-06-22', '2020-06-23', '2020-06-24', '2020-06-25', '2020-06-26', '2020-06-27', '2020-06-28', '2020-06-29', '2020-06-30', '2020-07-01', '2020-07-02', '2020-07-03', '2020-07-04', '2020-07-05', '2020-07-06', '2020-07-07', '2020-07-08', '2020-07-09', '2020-07-10', '2020-07-11', '2020-07-12', '2020-07-13', '2020-07-14', '2020-07-15', '2020-07-16', '2020-07-17', '2020-07-18', '2020-07-19'
-            ],
-            colors: ["#206bc4"],
-            legend: {
+            axisBorder: {
                 show: false,
             },
-        })).render();
-    });
-    // @formatter:on
+            type: 'datetime',
+        },
+        yaxis: {
+            labels: {
+                padding: 4
+            },
+        },
+        labels: [
+            '2020-06-20', '2020-06-21', '2020-06-22', '2020-06-23', '2020-06-24', '2020-06-25', '2020-06-26', '2020-06-27', '2020-06-28', '2020-06-29', '2020-06-30', '2020-07-01', '2020-07-02', '2020-07-03', '2020-07-04', '2020-07-05', '2020-07-06', '2020-07-07', '2020-07-08', '2020-07-09', '2020-07-10', '2020-07-11', '2020-07-12', '2020-07-13', '2020-07-14', '2020-07-15', '2020-07-16', '2020-07-17', '2020-07-18', '2020-07-19'
+        ],
+        colors: ["#206bc4"],
+        legend: {
+            show: false,
+        },
+    };
+
 </script>
 
 <div class="card">
@@ -90,5 +104,5 @@
             </div>
         </div>
     </div>
-    <div id="chart-revenue-bg" class="chart-sm"></div>
+    <div class="chart-sm" use:renderApexChart={chartRevenueBg}></div>
 </div>
