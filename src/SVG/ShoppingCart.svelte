aa49bfbd5ec124c803937d3992f927f9224439f4
Joan Andrés <andres6936@live.com>
2021-02-09 22:18:21 -0500

    Use of Svg24

--- src/SVG/ShoppingCart.svelte
+++ src/SVG/ShoppingCart.svelte
@@ -1,10 +1,10 @@
-<svg xmlns="http://www.w3.org/2000/svg" class="icon"
-     width="24" height="24" viewBox="0 0 24 24"
-     stroke-width="2" stroke="currentColor"
-     fill="none" stroke-linecap="round"
-     stroke-linejoin="round">
+<script>
+    import Svg24 from "./Svg24.svelte";
+</script>
+
+<Svg24>
     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
     <circle cx="9" cy="19" r="2"/>
     <circle cx="17" cy="19" r="2"/>
     <path d="M3 3h2l2 12a3 3 0 0 0 3 2h7a3 3 0 0 0 3 -2l1 -7h-15.2"/>
-</svg>
+</Svg24>
