b09ab5f077c0777dd5e42963f2b144e70d3aed18
Joan Andrés <andres6936@live.com>
2021-02-09 21:57:24 -0500

    Use of Svg24 tag

--- src/Component/PageTitle.svelte
+++ src/Component/PageTitle.svelte
@@ -1,3 +1,7 @@
+<script>
+    import Svg24 from "../SVG/Svg24.svelte";
+</script>
+
 <div class="page-header d-print-none">
     <div class="row align-items-center">
         <div class="col">
@@ -19,24 +23,20 @@
                   </span>
                 <a href="#" class="btn btn-primary d-none d-sm-inline-block" data-bs-toggle="modal"
                    data-bs-target="#modal-report">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
-                         viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                         stroke-linecap="round" stroke-linejoin="round">
+                    <Svg24>
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <line x1="12" y1="5" x2="12" y2="19"/>
                         <line x1="5" y1="12" x2="19" y2="12"/>
-                    </svg>
+                    </Svg24>
                     Create new report
                 </a>
                 <a href="#" class="btn btn-primary d-sm-none btn-icon" data-bs-toggle="modal"
                    data-bs-target="#modal-report" aria-label="Create new report">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
-                         viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                         stroke-linecap="round" stroke-linejoin="round">
+                    <Svg24>
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <line x1="12" y1="5" x2="12" y2="19"/>
                         <line x1="5" y1="12" x2="19" y2="12"/>
-                    </svg>
+                    </Svg24>
                 </a>
             </div>
         </div>
